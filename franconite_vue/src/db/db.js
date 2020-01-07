692aae0176d3499c59d76ae3a443afa2367849a6
kstrckr <ksphotopro@gmail.com>
2020-01-06 21:47:11 -0500

    moved app.js into src dir and updated dockerfile appropriately

---
+++ franconite_vue/src/db/db.js
@@ -0,0 +1,11 @@
+const projectsModels = [
+  {
+    title: 'Python Data',
+    imgName: 'sales_vs_vol_vs_pop.png' ,
+    introTxt: 'Database design, seeding, and database analysis of Iowa state Liquor Sales using Anaconda and Bokeh.',
+    githubUrl: 'https://github.com/kstrckr/python_data_project',
+    repoDisplayName: 'Python_Data',
+  }
+]
+
+module.exports = projectsModels;
--- franconite_vue/src/server/app.js
+++ franconite_vue/src/server/app.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const db = require('../db/db.js');
+
 const express = require('express');
 
 const PORT = 8080
@@ -12,6 +14,10 @@ app.use(express.static('dist'));
 app.get('/', (req, res) => {
   res.render('index.html');
 });
+
+app.get('/api/projects', (req, res) => {
+  res.json(db);
+})
 
 app.listen(PORT, HOST);
 
